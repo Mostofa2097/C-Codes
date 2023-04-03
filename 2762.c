9affa738952eea998cd5a88b81313466cf98cc46
Mostofa Kamal <mostofakamal3636@gmail.com>
2023-04-03 23:27:55 +0600

    c problem

---
+++ 2762.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+   char a,c,b;
+   scanf("%c%c%c",&a,&c,&b);
+   printf("%c %c %c\n",b,c,a);
+   return 0;
+
+
+}
---
+++ 2763s.c
@@ -0,0 +1,12 @@
+
+
+#include <stdio.h>
+ int main(){
+ int a1,a2,a3,a4;
+ scanf("%d.%d.%d-%d",&a1,&a2,&a3,&a4);
+ printf("%03d\n",a1);
+ printf("%03d\n",a2);
+ printf("%03d\n",a3);
+ printf("%02d\n",a4);
+ return 0;
+}
