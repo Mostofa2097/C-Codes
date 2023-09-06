c121df5d083623fd539ab52c936153d18d2a93e0
Mostofa Kamal <mostofakamal3636@gmail.com>
2023-09-06 14:21:08 +0600

    tree

---
+++ loop/112.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void main ()
+{
+
+    short tamaho, i, j, z;
+
+    while (scanf("%hu", &tamaho) != EOF)
+    {
+
+        for (i = 0; i < tamaho; i += 2)
+        {
+            for (j = 0; j < (tamaho - i) / 2; j++)
+                printf(" ");
+
+            for (z = i; z >= 0; z--)
+                printf("*");
+
+            printf("\n");
+
+        }
+
+        
+
+    }
+}
